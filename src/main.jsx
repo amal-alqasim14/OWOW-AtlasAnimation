00e33023476320858c29d3af5c8ee01020d6c126
Pierre Reumkens <506415@student.fontys.nl>
2026-05-21 12:42:40 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Router>
         <Routes>
           <Route path="/" element={<App />} />
+          <Route path="/whatever" element={<whatever />} />
         </Routes>
       </Router>
       {/* <StrictMode>
