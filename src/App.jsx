3da018c96fe595f7790cf5c845fd719199d5e48d
amal-alqasim14 <560205@student.fontys.nl>
2026-05-26 14:43:53 +0200

    home page toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Home from './Pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate maiores soluta nobis obcaecati rem eius nesciunt qui, impedit sapiente voluptates illo rerum inventore id aperiam quis et eaque animi ratione repellendus amet debitis, saepe eum? Est optio inventore aliquam aut illum veritatis error doloremque ipsam, nemo, iure similique tempore iusto? Nam pariatur deserunt aspernatur, quod, atque voluptatibus cumque totam sunt, dolore distinctio provident autem sed ex cum ducimus. Alias ea in porro sapiente odit, esse culpa earum assumenda dolorem dolore, fugit commodi laborum ab temporibus eligendi illo asperiores. Molestiae consectetur, quod similique vel est itaque ratione ea deleniti maiores sequi temporibus praesentium, tempore eveniet delectus quisquam aspernatur reprehenderit nisi, rerum accusantium iusto quibusdam! Doloribus accusamus blanditiis hic molestias magni nostrum voluptate natus magnam cumque possimus nihil sunt, distinctio, laudantium eaque numquam. Molestiae vero perferendis sit, ipsa voluptatum tempore enim, expedita architecto, cumque aut distinctio sapiente est soluta atque consequuntur id dicta? Id error consectetur porro nobis saepe molestiae facilis a nihil officiis quibusdam, quos soluta quam iure quod mollitia non tempore! Libero suscipit blanditiis possimus alias obcaecati tempora laborum reiciendis dolorum cum amet architecto ipsum, perspiciatis voluptates inventore quibusdam quisquam delectus, magni nobis aliquid, quod consequuntur quas quam. Veniam magnam, officia placeat perspiciatis numquam porro mollitia odio tempore aliquam quasi voluptatum ipsum molestias neque eos nemo sed assumenda eveniet fugiat. Quos commodi maiores ab! Dolores exercitationem ab sed, esse voluptatem aut illum mollitia quas numquam cumque perferendis officia earum molestias, nesciunt sint ullam quam consequatur impedit soluta, doloribus id distinctio neque? Accusantium excepturi adipisci doloribus accusamus nam vitae voluptatem culpa perspiciatis quibusdam, impedit assumenda minima modi consequuntur saepe tempore, quisquam voluptas, dignissimos expedita aut cum temporibus perferendis natus! Aperiam ad nam deleniti modi, illum earum molestiae quas eligendi repudiandae fugiat enim similique magni, minima quaerat vitae quos, alias rem sunt.
-    </>
+    <Home/>
   )
 }
 
