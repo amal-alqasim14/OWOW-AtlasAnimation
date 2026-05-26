0df25a849bb8dbdc0e9cbd03dbc5532835facad8
Pierre Reumkens <506415@student.fontys.nl>
2026-05-26 10:19:36 +0200

    animations

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,6 @@ createRoot(document.getElementById('root')).render(
       <Router>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/whatever" element={<whatever />} />
         </Routes>
       </Router>
       {/* <StrictMode>
