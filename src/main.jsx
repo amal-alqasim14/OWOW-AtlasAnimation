e21e3e10f9554be56677b369128492e2fba3cfa9
amal-alqasim14 <560205@student.fontys.nl>
2026-05-26 13:10:46 +0200

    Category pages toegevoegd

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import './index.css'
+
 import { Frame } from './Components/Frame.jsx'
 import App from './App.jsx'
+import Category from './Components/category.jsx'
 import Details from './Components/Details.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <>
-    <h1 id="search-filter-values" />
-    <Frame />
-    <div id="content">
-      <Router>
+  <StrictMode>
+    <Router>
+
+      <h1 id="search-filter-values"></h1>
+
+      <Frame />
+
+      <div id="content">
         <Routes>
           <Route path="/" element={<App />} />
+          <Route path="/category" element={<Category />} />
           <Route path="/details" element={<Details />} />
         </Routes>
-      </Router>
-      {/* <StrictMode>
-        <App />
-      </StrictMode> */}
-    </div>
-  </>
+      </div>
+
+    </Router>
+  </StrictMode>
 )
